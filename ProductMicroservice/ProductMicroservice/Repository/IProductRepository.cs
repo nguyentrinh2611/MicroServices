f4d36b7a80f9ec95bc2f1c897d4a010908e3f3bb
nguyentrinha3qt <nguyentrinha3qt@gmail.com>
2024-01-01 14:10:39 +0700

    add baitap

---
+++ ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductMicroservice.Models;
+using System.Collections.Generic;
+
+namespace ProductMicroservice.Repository
+{
+  public  interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int ProductId);
+        void InsertProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void UpdateProduct(Product Product);
+        void Save();
+    }
+}
---
+++ ProductMicroservice/ProductMicroservice/Repository/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using ProductMicroservice.DBContexts;
+using ProductMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ProductMicroservice.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+        public Product GetProductById(int ProductId)
+        {
+            return _dbContext.Products.Find(ProductId);
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public Product GetProductByID(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
